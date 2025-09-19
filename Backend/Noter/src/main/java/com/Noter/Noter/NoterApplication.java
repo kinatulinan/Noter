d7728f4142dde52476003d2bc120d914b2f268bd
kinatulinan <151549802+kinatulinan@users.noreply.github.com>
2025-09-19 10:02:01 +0800

    Noter

---
+++ Backend/Noter/src/main/java/com/Noter/Noter/NoterApplication.java
@@ -0,0 +1,13 @@
+package com.Noter.Noter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoterApplication.class, args);
+	}
+
+}
---
+++ Backend/Noter/src/test/java/com/Noter/Noter/NoterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Noter.Noter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
